cf8953ea11b9bf0297b29cbf72bdcd22e1097ab3
LtAden <64529736+LtAden@users.noreply.github.com>
2021-11-23 10:26:07 +0100

    Add files via upload
    initial commit

---
+++ src/test/java/com/github/LtAden/qiagenproject/QiagenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.LtAden.qiagenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiagenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
